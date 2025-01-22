d3497f207aa7d052b2728b64aed4ae809d7ed110
Guillermo Theler <guillermot@onesolutions.com.ar>
2025-01-22 17:21:31 -0300

    compileall script

--- compileAll.ps1
+++ compileAll.ps1
@@ -1,8 +1,7 @@
-# [System.Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\AdoptOpenJDK\jdk-8.0.265.01-hotspot")
-#[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "D:\GTheler\prog\Eclipse Adoptium\jdk8u332-b09")
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "D:\prog\_JAVA\jdk8u332-b09")
+$ORIGINAL_JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME")
 
-# cd C:\Users\guill\prog\java_mvn\ppbridge
-# SET JAVA_HOME=$GCEMPOS_JAVA_HOME
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", [System.Environment]::GetEnvironmentVariable("JAVA17_HOME"))
 
 mvn clean install
+
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", $ORIGINAL_JAVA_HOME)
